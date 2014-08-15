b03f53b216963def8ca9b23e13f294cb13987df3
Andi Altendorfer <andreas@altendorfer.at>
2014-08-15 10:43:23 +0200

    Added _vimrc and modified specs

--- project_source/spec/spec_helper.rb
+++ project_source/spec/spec_helper.rb
@@ -2,24 +2,24 @@ require 'rspec/given'
 
 RSpec.configure do |config|
 
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
+config.filter_run :focus
+config.run_all_when_everything_filtered = true
 
-  if config.files_to_run.one?
-    config.default_formatter = 'doc'
-  end
+if config.files_to_run.one?
+config.default_formatter = 'doc'
+end
 
-  config.profile_examples = 10
-  config.order = :random
-  Kernel.srand config.seed
+config.profile_examples = 10
+config.order = :random
+Kernel.srand config.seed
 
-  config.expect_with :rspec do |expectations|
-    expectations.syntax = :expect
-  end
+config.expect_with :rspec do |expectations|
+expectations.syntax = :expect
+end
 
-  config.mock_with :rspec do |mocks|
-    mocks.syntax = :expect
-    mocks.verify_partial_doubles = true
-  end
+config.mock_with :rspec do |mocks|
+mocks.syntax = :expect
+mocks.verify_partial_doubles = true
+end
 
 end
